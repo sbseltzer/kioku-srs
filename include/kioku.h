d13a907d7fbe7188deecdc459b50048b1b6a5143
Samuel Seltzer-Johnston <you@example.com>
2017-10-04 13:46:32 -0400

    Couldn't hurt to add these ifdef gates

--- include/kioku.h
+++ include/kioku.h
@@ -1,6 +1,11 @@
+#ifndef _KIOKU_H
+#define _KIOKU_H
+
 #include "kioku/types.h"
 #include "kioku/log.h"
 #include "kioku/model.h"
 #include "kioku/controller.h"
 #include "kioku/server.h"
 #include "kioku/rest.h"
+
+#endif /* _KIOKU_H */
