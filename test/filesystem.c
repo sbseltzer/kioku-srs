37fdc970ef407d8dd0ef17acb86f0b0581b36bf1
Samuel Seltzer-Johnston <you@example.com>
2017-10-08 12:19:40 -0400

    Added diag messages for the segfault check

--- test/filesystem.c
+++ test/filesystem.c
@@ -184,7 +184,9 @@ void test_concat_path()
   cmp_ok(strlen(expected_path), "==", got_len);
   cmp_ok(got_len, ">", DESTSIZE - 1);
   /* This one is a reasonably portable way to try and tease out a segfault */
+  diag("Will it segfault?");
   got_len = kioku_path_concat((char*)-1, 0, path1, path2);
+  diag("Nope. Hooray!");
   cmp_ok(strlen(expected_path), "==", got_len);
   cmp_ok(got_len, ">", DESTSIZE - 1);
 
