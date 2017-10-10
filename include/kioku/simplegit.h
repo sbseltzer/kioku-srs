75350df88e647f9812986bf76c764ed37430ec40
Samuel Seltzer-Johnston <you@example.com>
2017-10-10 17:11:28 -0400

    Exposed first git API method

--- include/kioku/simplegit.h
+++ include/kioku/simplegit.h
@@ -7,4 +7,13 @@ typedef struct srsGIT_CREATE_OPTS_s {
   const char *first_commit_message;
 } srsGIT_CREATE_OPTS;
 
+#define srsGIT_CREATE_OPTS_DEFAULT {".gitignore", "", "Initial Commit"}
+
+/** Create a repository with a first file and initial commit.
+ * \param[in] path The path at which to create it, which represents a directory, and serve as the home of the .git folder.
+ * \param[in] opts The options that specify how the repository is initialized. If NULL, it will be substituted with srsGIT_CREATE_OPTS_DEFAULT. If non-NULL but with some NULL parameters, the NULL parameters will be substituted with the corresponding value in srsGIT_CREATE_OPTS_DEFAULT.
+ * \return Whether creation was successful.
+ */
+kiokuAPI bool srsGit_Repo_Create(const char *path, const srsGIT_CREATE_OPTS *opts);
+
 #endif /* _KIOKU_SIMPLEGIT_H */
