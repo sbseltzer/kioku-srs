998347e99e407398bab45ead835ec042c73f1e20
Samuel Seltzer-Johnston <you@example.com>
2017-10-20 14:38:07 -0400

    New API method declarations for getting card information

--- include/kioku/model.h
+++ include/kioku/model.h
@@ -28,6 +28,26 @@
  */
 kiokuAPI bool srsModel_Card_GetNextID(const char *deck_path, char *card_id_out, size_t card_id_out_size);
 
+/**
+ * Outputs the card path to the specified card ID within the specified deck. Returns false if it fails to find it for whatever reason.
+ * @param[in] deck_path Path to the deck to look in.
+ * @param[in] card_id The ID of the card to get the content of.
+ * @param[out] path_out Buffer to store the the path in, including null-terminator. The contents are unspecified if this function returns false.
+ * @param[in] path_size The size of the @ref path_out buffer including the null-terminator. If there's not enough room, this function will return false.
+ * @return Whether the path could be stored. False could indicate a bad parameter, an invalid deck, an invalid card ID, or a buffer that's not large enough according to the specified size. @todo Failure ambiguity is a design flaw. For now the user is expected to use @ref kiokuPATH_MAX + 1 to protect themselves.
+ */
+kiokuAPI bool srsModel_Card_GetPath(const char *deck_path, const char *card_id, char *path_out, size_t path_size);
+
+/**
+ * Outputs the content of the next scheduled card in the specified deck path.
+ * @param[in] deck_path Path to the deck to look in.
+ * @param[in] card_id The ID of the card to get the content of. It will only be considered valid if a file @ref srsModel_Card_GetPath gives back an existing path.
+ * @param[out] buf Buffer to store the content in, if any. The contents are unspecified if this function returns false.
+ * @param[in] buf_size Size of the buffer to store the ID in, including the null-terminator. To ensure there's enough room, use the length of the file content plus one.
+ * @return True if a the content is available for the specified deck/card ID, and the content size did not exceed the provided buffer size. @todo Failure ambiguity is a design flaw. For now the user should use @ref kioku_filesystem_getlen to protect themselves.
+ */
+kiokuAPI bool srsModel_Card_GetCardContent(const char *deck_path, const char *card_id, const char *buf, size_t buf_size);
+
 bool kioku_model_init(uint32_t argc, char **argv);
 void kioku_model_exit();
 
