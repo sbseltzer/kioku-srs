5a6efd3aee9f1cbce5fd087237f0b770f5965bdf
Samuel Seltzer-Johnston <you@example.com>
2017-10-20 13:51:42 -0400

    Documented srsModule_Card_GetNextID

--- include/kioku/model.h
+++ include/kioku/model.h
@@ -19,6 +19,13 @@
 
 #define srsMODEL_CARD_ID_MAX 256
 
+/**
+ * Get the next card ID for a deck.
+ * @param[in] deck_path Path to the deck to get the next card ID of.
+ * @param[out] card_id_buf Place to store the ID string. Must be large enough to include a null-terminator.
+ * @param[in] card_id_buf_size Size of the buffer, including the null terminator.
+ * @return Whether the card ID could be stored. False could indicate a bad parameter, an invalid deck, a deck with no next card, or a buffer that's not large enough according to the specified size.
+ */
 kiokuAPI bool srsModel_Card_GetNextID(const char *deck_path, char *card_id_buf, size_t card_id_buf_size);
 
 bool kioku_model_init(uint32_t argc, char **argv);
