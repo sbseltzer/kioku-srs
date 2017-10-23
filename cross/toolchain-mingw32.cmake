23ded35b5199ce3c9a462eb008a202cf499a12a2
Samuel Seltzer-Johnston <you@example.com>
2017-10-23 14:12:03 -0400

    Make sure the MinGW DLLTOOL can be found

--- cross/toolchain-mingw32.cmake
+++ cross/toolchain-mingw32.cmake
@@ -8,3 +8,6 @@ SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc )
+
+# the embedded winhttp in libgit2 fails to find the MinGW version of dlltool
+SET(DLLTOOL i586-mingw32msvc-dlltool)
--- cross/toolchain-mingw64.cmake
+++ cross/toolchain-mingw64.cmake
@@ -5,6 +5,10 @@ SET(CMAKE_SYSTEM_NAME Windows)
 SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-gcc)
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH  /usr/i686-w64-mingw32/ )
+
+# the embedded winhttp in libgit2 fails to find the MinGW version of dlltool
+SET(DLLTOOL i686-w64-mingw32-dlltool)
