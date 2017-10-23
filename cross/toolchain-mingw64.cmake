914e8aa55626e806c6baa4ed11378bbf187a4254
Samuel Seltzer-Johnston <you@example.com>
2017-10-23 13:33:12 -0400

    Changed build strategy to toolchain specification for MinGW

---
+++ cross/toolchain-mingw64.cmake
@@ -0,0 +1,10 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER i686-w64-mingw32-gcc)
+SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH  /usr/i686-w64-mingw32/ )
