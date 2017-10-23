e0aa2a563d2eec371594a9923439e9ac0b62eada
Samuel Seltzer-Johnston <you@example.com>
2017-10-23 17:29:38 -0400

    Try changing these and see what happens

--- cross/toolchain-mingw32.cmake
+++ cross/toolchain-mingw32.cmake
@@ -2,8 +2,8 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
+SET(CMAKE_C_COMPILER i586-mingw32msvc-cc)
+SET(CMAKE_CXX_COMPILER i586-mingw32msvc-c++)
 SET(CMAKE_RC_COMPILER i586-mingw32msvc-windres)
 
 # here is the target environment located
