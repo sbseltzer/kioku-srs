c87e4cac2af4b5b2cbc8d421df3f9d51a5bd4676
Sam Seltzer-Johnston <samuel.seltzer-johnston@movimentogroup.com>
2017-10-27 17:00:43 -0400

    Initializing fullpath size without that extra byte

--- src/kioku/simplegit.c
+++ src/kioku/simplegit.c
@@ -346,7 +346,7 @@ bool srsGit_Add(const char *path)
   }
 
   const char *repo_path = srsGit_Repo_GetCurrent();
-  char fullpath[kiokuPATH_MAX+1];
+  char fullpath[kiokuPATH_MAX] = {0};
   size_t fullpath_len = kioku_path_getfull(path, fullpath, sizeof(fullpath));
   size_t str_index = 0;
   if (strncmp(repo_path, fullpath, strlen(repo_path)) < 0)
