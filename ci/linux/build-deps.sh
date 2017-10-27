680541ebc6d9935fbb1411de187c14f8392ce4ad
Samuel Seltzer-Johnston <you@example.com>
2017-10-27 10:26:47 -0400

    Compile dependencies with PIC

--- ci/linux/build-deps.sh
+++ ci/linux/build-deps.sh
@@ -20,7 +20,7 @@ mkdir build
 # Attempt to go to build dir and clear it out if it has anything in it.
 cd build && make clean && rm -rf *
 # Build the project
-cmake .. -G"$build_type" -DCMAKE_INSTALL_PREFIX:PATH=$build_dir/extern/libssh2/build/src -DBUILD_SHARED_LIBS=OFF
+cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX:PATH=$build_dir/extern/libssh2/build/src -DBUILD_SHARED_LIBS=OFF
 cmake --build .
 cmake --build . --target install
 ls src
@@ -34,7 +34,7 @@ mkdir build
 # Attempt to go to build dir and clear it out if it has anything in it.
 cd build && rm -rf *
 # Build the project
-PKG_CONFIG_PATH=$build_dir/extern/libssh2/build/src cmake .. -G"$build_type" -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF
+PKG_CONFIG_PATH=$build_dir/extern/libssh2/build/src cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF
 cmake --build .
 ls
 
