27486cd6ff4c1ed841b1ce04494adf04558dba3f
Sam Seltzer-Johnston <samuel.seltzer-johnston@movimentogroup.com>
2017-10-28 11:05:40 -0400

    Allow both shared or static builds in the linux scripts, defaulting to shared.

--- ci/linux/build-deps.sh
+++ ci/linux/build-deps.sh
@@ -7,6 +7,7 @@ result=0
 : ${TRAVIS_BUILD_DIR:=$start_dir}
 build_dir=$TRAVIS_BUILD_DIR
 build_type="Unix Makefiles"
+build_shared=OFF
 
 echo "Start dir: $start_dir"
 echo "Build dir: $build_dir"
@@ -21,7 +22,7 @@ mkdir build
 # Attempt to go to build dir and clear it out if it has anything in it.
 cd build && make clean && rm -rf *
 # Build the project
-cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX:PATH=$build_dir/extern/libssh2/build/src -DBUILD_SHARED_LIBS=OFF
+cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX:PATH=$build_dir/extern/libssh2/build/src -DBUILD_SHARED_LIBS=$build_shared
 cmake --build . --config $build_conf
 cmake --build . --target install
 ls src
@@ -35,7 +36,7 @@ mkdir build
 # Attempt to go to build dir and clear it out if it has anything in it.
 cd build && rm -rf *
 # Build the project
-PKG_CONFIG_PATH=$build_dir/extern/libssh2/build/src cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF
+PKG_CONFIG_PATH=$build_dir/extern/libssh2/build/src cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=$build_shared
 cmake --build . --config $build_conf
 ls
 
--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -2,11 +2,16 @@
 # Initialize variables
 start_dir=$PWD
 result=0
-# : ${lib_ext:="so"}
 : ${build_conf:="Release"}
 : ${TRAVIS_BUILD_DIR:=$start_dir}
 build_dir=$TRAVIS_BUILD_DIR
 build_type="Unix Makefiles"
+: ${build_shared:=ON}
+if ! "x$build_shared" == "xON"; then
+    lib_ext="so"
+else
+    lib_ext="a"
+fi
 
 echo "Start dir: $start_dir"
 echo "Build dir: $build_dir"
@@ -19,20 +24,21 @@ mkdir build
 # Attempt to go to build dir and clear it out if it has anything in it.
 cd build && make clean && rm -rf *
 # Copy libraries over
-# cp $build_dir/extern/libssh2/build/src/*.$lib_ext* .
-# cp $build_dir/extern/libgit2/build/*.$lib_ext* .
-# Check whether they were copied
-# if ! test -f libgit2.$lib_ext ; then
-#     printf '%s\n' 'Build: Failed to copy libgit2!' >&2
-#     result=1
-# elif ! test -f libssh2.$lib_ext ; then
-#     printf '%s\n' 'Build: Failed to copy libssh2!' >&2
-#     result=1
-# else
+if ! "x$build_shared" == "xON"; then
+  cp $build_dir/extern/libssh2/build/src/*.$lib_ext* .
+  cp $build_dir/extern/libgit2/build/*.$lib_ext* .
+  #Check whether they were copied
+  if ! test -f libgit2.$lib_ext ; then
+      printf '%s\n' 'Build: Failed to copy libgit2!' >&2
+      result=1
+  elif ! test -f libssh2.$lib_ext ; then
+      printf '%s\n' 'Build: Failed to copy libssh2!' >&2
+      result=1
+  fi
+fi
 cmake .. -G"$build_type"
 cmake --build . --config $build_conf
 CTEST_OUTPUT_ON_FAILURE=1 ctest -C $build_conf
 result=$?
-# fi
 cd $start_dir
 exit $result
