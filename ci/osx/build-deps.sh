4eb2169babe758ab6fd8a9f710a377ab2d081a1b
Samuel Seltzer-Johnston <you@example.com>
2017-10-28 13:14:16 -0400

    Possible fix based on suggestion: https://github.com/rust-lang/cargo/issues/2295

--- ci/osx/build-deps.sh
+++ ci/osx/build-deps.sh
@@ -21,7 +21,7 @@ mkdir build
 # Attempt to go to build dir and clear it out if it has anything in it.
 cd build && make clean && rm -rf *
 # Build the project
-cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX:PATH=$build_dir/extern/libssh2/build/src -DBUILD_SHARED_LIBS=OFF
+cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX:PATH=$build_dir/extern/libssh2/build/src -DBUILD_SHARED_LIBS=OFF -DOPENSSL_ROOT_DIR=$(brew --prefix openssl)
 cmake --build . --config $build_conf
 cmake --build . --target install
 ls src
@@ -35,7 +35,7 @@ mkdir build
 # Attempt to go to build dir and clear it out if it has anything in it.
 cd build && rm -rf *
 # Build the project
-PKG_CONFIG_PATH=$build_dir/extern/libssh2/build/src cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF
+PKG_CONFIG_PATH=$build_dir/extern/libssh2/build/src cmake .. -G"$build_type" -DCMAKE_CXXFLAGS="-fPIC" -DCMAKE_C_FLAGS="-fPIC" -DBUILD_CLAR=OFF -DBUILD_SHARED_LIBS=OFF -DOPENSSL_ROOT_DIR=$(brew --prefix openssl)
 cmake --build . --config $build_conf
 ls
 
