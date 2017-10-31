c30a3d65372099cce5e5e63a6fe41c41224ef0a9
Samuel Seltzer-Johnston <you@example.com>
2017-10-31 12:36:30 -0400

    Added BUILD_SERVER option to speed up build by not building server

--- ci/linux/build.sh
+++ ci/linux/build.sh
@@ -8,6 +8,7 @@ result=0
 build_dir=$TRAVIS_BUILD_DIR
 build_type="Unix Makefiles"
 : ${build_shared:=ON}
+: ${build_server:=ON}
 : ${install_prefix:="$build_dir/extern/install"}
 if test "x$build_shared" = "xON"; then
     lib_ext="so"
@@ -40,7 +41,7 @@ if test "x$build_shared" = "xON"; then
       result=1
   fi
 fi
-PKG_CONFIG_PATH=$install_prefix/lib/pkgconfig:$install_prefix/lib64/pkgconfig cmake .. -G"$build_type" -DBUILD_SHARED_LIBS=$build_shared -DCMAKE_BUILD_TYPE=$build_conf
+PKG_CONFIG_PATH=$install_prefix/lib/pkgconfig:$install_prefix/lib64/pkgconfig cmake .. -G"$build_type" -DBUILD_SHARED_LIBS=$build_shared -DCMAKE_BUILD_TYPE=$build_conf -DBUILD_SERVER=$build_server
 cmake --build . --config $build_conf
 CTEST_OUTPUT_ON_FAILURE=1 ctest -C $build_conf
 result=$?
