447f84ed3a851e73066f2e5454e36fad36d3e764
Sam <sam@sbseltzer.net>
2017-10-31 23:28:06 -0400

    Logs always flush.

--- include/kioku/log.h
+++ include/kioku/log.h
@@ -20,28 +20,31 @@ kiokuAPI FILE *kioku_log_GetHandle();
 
 kiokuAPI void kioku_log_Exit();
 
-#define kLOG_WRITE(...)                                       \
-  do {                                                             \
-    fprintf(kioku_log_GetHandle(), "%s:%d: ", __FILE__, __LINE__); \
-    fprintf(kioku_log_GetHandle(), __VA_ARGS__);              \
-    fprintf(kioku_log_GetHandle(), "\r\n");                        \
-  } while (0)
+#define kLOG_WRITE(...)                                           \
+  do {                                                            \
+  fprintf(kioku_log_GetHandle(), "%s:%d: ", __FILE__, __LINE__);  \
+  fprintf(kioku_log_GetHandle(), __VA_ARGS__);                    \
+  fprintf(kioku_log_GetHandle(), "\r\n");                         \
+  fflush(kioku_log_GetHandle());                                  \
+} while (0)
 
-#define srsLOG_NOTIFY(...)                      \
-  do {                                              \
-    fprintf(stdout, "%s:%d: ", __FILE__, __LINE__); \
-    fprintf(stdout, __VA_ARGS__);              \
-    fprintf(stdout, "\r\n");                        \
-    kLOG_WRITE(__VA_ARGS__);                   \
-  } while (0)
+#define srsLOG_NOTIFY(...)                            \
+    do {                                              \
+      fprintf(stdout, "%s:%d: ", __FILE__, __LINE__); \
+      fprintf(stdout, __VA_ARGS__);                   \
+      fprintf(stdout, "\r\n");                        \
+      fflush(stdout);                                 \
+      kLOG_WRITE(__VA_ARGS__);                        \
+    } while (0)
 
-#define srsLOG_ERROR(...)                                      \
-  do {                                                              \
-    fprintf(stderr, "%s:%d: ", __FILE__, __LINE__);                 \
-    fprintf(stderr, __VA_ARGS__);                              \
-    fprintf(stderr, "\r\n");                                        \
-    kLOG_WRITE(__VA_ARGS__);                                   \
-  } while (0)
+#define srsLOG_ERROR(...)                             \
+    do {                                              \
+      fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
+      fprintf(stderr, __VA_ARGS__);                   \
+      fprintf(stderr, "\r\n");                        \
+      fflush(stderr);                                 \
+      kLOG_WRITE(__VA_ARGS__);                        \
+    } while (0)
 
 #endif /* _KIOKU_LOG_H */
 
