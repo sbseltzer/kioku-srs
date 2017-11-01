fbdcd34a021f967be239f85c82785980f08ec556
Sam <sam@sbseltzer.net>
2017-10-31 23:40:52 -0400

    Totally forgot to ADD the string module and associated tests...

---
+++ test/string.c
@@ -0,0 +1,67 @@
+#include "greatest.h"
+#include "kioku/string.h"
+
+void test_number(const char *str, bool isnum, int32_t num)
+{
+  int32_t number = 0;
+  ASSERT_EQ(isnum, srsString_ToU32(str, NULL));
+  ASSERT_EQ(isnum, srsString_ToU32(str, &number));
+  if (isnum)
+  {
+    ASSERT_EQ(num, number);
+  }
+}
+
+/* A test runs various assertions, then calls PASS(), FAIL(), or SKIP(). */
+TEST TestStringToU32(void)
+{
+  int32_t number = 0;
+
+  ASSERT_FALSE(srsString_ToU32(NULL, NULL));
+  ASSERT_FALSE(srsString_ToU32(NULL, &number));
+
+  test_number("0",  true, 0);
+  test_number("0abc",  true, 0);
+  test_number("000", true, 0);
+  test_number("000abc", true, 0);
+
+  test_number("1",  true, 1);
+  test_number("1abc",  true, 1);
+  test_number("12345", true, 12345);
+  test_number("12345abc", true, 12345);
+
+  test_number("-1", true, 1);
+  test_number("-1abc", true, 1);
+  test_number("-12345", true, -12345);
+  test_number("-12345abc", true, -12345);
+
+  test_number("1111111111111111111111111111111111111111111", false, 0);
+  test_number("-1111111111111111111111111111111111111111111", false, 0);
+
+  test_number("abc", false, 0);
+  test_number("-abc", false, 0);
+  test_number("", false, 0);
+
+  PASS();
+}
+
+
+/* Suites can group multiple tests with common setup. */
+SUITE(the_suite) {
+  RUN_TEST(TestStringToU32);
+}
+
+/* Add definitions that need to be in the test runner's main file. */
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char **argv) {
+  GREATEST_MAIN_BEGIN();      /* command-line options, initialization. */
+
+  /* Individual tests can be run directly. */
+  /* RUN_TEST(x_should_equal_1); */
+
+  /* Tests can also be gathered into test suites. */
+  RUN_SUITE(the_suite);
+
+  GREATEST_MAIN_END();        /* display results */
+}
