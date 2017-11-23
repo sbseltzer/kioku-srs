23b4e1b7389c955ae7dc3ce3823dc739a9a7b1b4
Sam Seltzer-Johnston <sam@sbseltzer.net>
2017-11-22 21:04:54 -0500

    Started drafting up ranged enums for module result codes

---
+++ include/kioku/enum.h
@@ -0,0 +1,44 @@
+#include "kioku/decl.h"
+#include "kioku/types.h"
+
+/* Adds min/max values to an enum to force an explicit underlying int type */
+#define srsENUM_ADD_BOUNDS(prefix, intkind)     \
+  prefix # _MIN = intkind # _MIN,               \
+    prefix # _MAX = intkind # _MAX
+
+/* Adds a numerical start point for an enum range */
+#define srsENUM_RANGE_START(prefix, index, separation)                  \
+  /* We subtract from one here so that values for a ranged enum start exactly on the separation boundary (i.e. 0, 1 * sep, 2 * sep, i * sep, ...) */ \
+  prefix # _RANGE_START = ((index * separation) - 1)
+
+/* Adds the numerical end point for an enum range, then calculates the number of values in the range */
+#define srsENUM_RANGE_END(prefix, index, enum_space)                    \
+  /* The endpoint of valid values for this range */                     \
+  prefix # _RANGE_END,                                                  \
+    /* The maximum possible endpoint for this range - used for assertion */ \
+    prefix # _RANGE_MAX = ((index) * (enum_space # _SEPARATION)),       \
+    /* The number of values in this range */                            \
+    prefix # _COUNT     = ((prefix # _RANGE_START) - (prefix # _RANGE_END)), \
+    /* Boundaries to keep all enums in the same enumeration space of ranges have the same underlying integer type */ \
+    prefix # _MIN       = (enum_space # _RANGES_MIN),                   \
+    prefix # _MAX       = (enum_space # _RANGES_MAX),                   \
+    /* Ubiquitous undefined/invalid value */                            \
+    prefix # _NONE      = -1
+
+/* Convert a ranged enum value from a named enumeration space to a value that can reasonably be used as an index into an array */
+#define srsENUM_ToIndex(value, enum_space) ((value) % (enum_space # _SEPARATION))
+
+/* Check if a value sits in a specific enumeration range */
+#define srsENUM_InRange(value, enum_prefix) ( ((value) > (enum_prefix # _RANGE_START)) && ((value) < (enum_prefix # _RANGE_END)) )
+
+/* Get the unique ID of the range within a enumeration space of ranges */
+#define srsENUM_GetRangeID(value, enum_space) (value / (enum_space # _SEPARATION))
+
+/* Assert that an enum has had its ranges defined properly */
+#define srsENUM_RANGE_CHECK(prefix, index)                              \
+  /* Assert that the range end is less than (not less than or equal to) the max range end for that part of the enumeration space */ \
+  srsASSERT_STATIC();                                                   \
+  /* Assert that the count is less than the separation value for the enumeration space */ \
+  srsASSERT_STATIC();                                                   \
+  /* Assert that the size */                                            \
+  srsASSERT_STATIC()
---
+++ include/kioku/result.h
@@ -0,0 +1,29 @@
+#include "kioku/enum.h"
+
+/* An enumeration space for module result codes */
+typedef enum _srsRESULT_MODULES_e
+{
+  srsMODULE_GENERIC = 0,
+  srsMODULE_FILESYSTEM,
+  srsMODULE_GIT,
+  srsMODULE_MODEL,
+  srsMODULE_DATASTRUCTURE,
+  /* Meta-values */
+  srsMODULE_COUNT,
+  srsMODULE_NONE = -1,
+  /* Each module has this many codes available in its result range */
+  srsMODULE_SEPARATION = 100
+  /* This bounds is special in that it is used by all the ranges registered for this enumeration space */
+  srsENUM_ADD_BOUNDS(srsMODULE_RANGES, INT32)
+} srsRESULT_MODULES;
+
+/* A generic result type that can be used across all enums modules enumeration space */
+typedef enum _srsRESULT_e
+{
+  srsENUM_RANGE_START(srsRESULT, srsMODULE_GENERIC, srsMODULE_SEPARATION),
+  srsRESULT_OK,                 /* All is well */
+  srsRESULT_BAD,                /* Generic NOT OK */
+  srsRESULT_ERROR,              /* Generic error */
+  srsRESULT_FATAL,              /* Show stopper */
+  srsENUM_RANGE_END(srsRESULT, srsMODULE_GENERIC, srsMODULE_SEPARATION),
+} srsRESULT;
