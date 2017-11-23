88f70a2e65ae7e8a7b7f781bb8ee592d2b6276f8
Sam Seltzer-Johnston <sam@sbseltzer.net>
2017-11-22 21:52:35 -0500

    Made room for captured errno

--- include/kioku/error.h
+++ include/kioku/error.h
@@ -16,23 +16,24 @@ typedef struct _srsERROR_DATA_s
   /* Extras */
   int32_t     log_line_number;  /* The line in the log file that was created when this error occurred. -1 if no associated log was printed. */
   void       *dynamic_userdata; /* Dynamically allocated error-specific userdata. Depends on the source. Generally left alone. */
+  int32_t     errno;            /* A captured errno */
 } srsERROR_DATA;
 
 kiokuAPI srsERROR_DATA srsError_Get();
 
 kiokuAPI void srsError_Reset();
 
-kiokuAPI void srsError_Set(srsRESULT code, const char *name, const char *message,
-                                    const char *_FILENAME, int32_t line_number, const char _FUNCNAME);
+kiokuAPI void srsError_Set(srsRESULT code, const char *name, const char *message, int32_t errno_capture,
+                           const char *_FILENAME, int32_t line_number, const char _FUNCNAME);
 
 kiokuAPI void srsError_SetExtra(int32_t log_line_number, void *userdata);
 
-#define srsERROR_SET(code, name_getter, msg)          \
-  do {                                                \
-    srsError_Reset();                                 \
-    const char *error_code_name = name_getter(code);  \
-    srsError_Set(code, error_code_name, message,      \
-                 __FILE__, __LINE__, __FUNC__);       \
+#define srsERROR_SET(code, name_getter, msg, errno_capture)     \
+  do {                                                          \
+    srsError_Reset();                                           \
+    const char *error_code_name = name_getter(code);            \
+    srsError_Set(code, error_code_name, message, errno_capture, \
+                 __FILE__, __LINE__, __FUNC__);                 \
   } while(0)
 
 /* This will take the last set error, create a log for it, then retroactively set its log line number. */
