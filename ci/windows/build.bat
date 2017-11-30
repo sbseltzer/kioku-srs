03791705d10d9d4b684ed456237632d3dbab0a84
Sam <sam@sbseltzer.net>
2017-11-29 19:51:17 -0500

    Added a line to see which version of CTest is on Appveyor (hangs may be a bug)

--- ci/windows/build.bat
+++ ci/windows/build.bat
@@ -58,6 +58,7 @@ if NOT EXIST git2.%lib_ext% (
 cd ..
 IF "%skip_test%"=="" (
    set CTEST_OUTPUT_ON_FAILURE=1
+   ctest --version
    ctest -C %build_conf% --timeout 5
    set result=%errorlevel%
    if not "%result%"=="0" (
