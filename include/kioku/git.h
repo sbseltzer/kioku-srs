92f16cf5e56c3d6bbf455c66d478f279d180a181
Sam Seltzer-Johnston <sam@sbseltzer.net>
2017-12-03 16:23:24 -0500

    Extra docs for git module

--- include/kioku/git.h
+++ include/kioku/git.h
@@ -6,7 +6,7 @@
 #include <kioku/debug.h>
 
 typedef struct srsGIT_CREATE_OPTS_s {
-  const char *first_file_name;
+  const char *first_file_name; /** This will be relative to the new repository */
   const char *first_file_content;
   const char *first_commit_message;
 } srsGIT_CREATE_OPTS;
@@ -16,7 +16,7 @@ typedef struct srsGIT_CREATE_OPTS_s {
 /**
  * Create a repository with a first file and initial commit.
  * This will update all git functions to operate on the new repository.
- * @param[in] path The path at which to create it, which represents a directory, and serve as the home of the .git folder.
+ * @param[in] path The path at which to create it, which represents a directory, and serve as the home of the .git folder. This can be a relative path.
  * @param[in] opts The options that specify how the repository is initialized. If NULL, it will be substituted with srsGIT_CREATE_OPTS_DEFAULT. If non-NULL but with some NULL parameters, the NULL parameters will be substituted with the corresponding value in srsGIT_CREATE_OPTS_DEFAULT.
  * @return Whether creation was successful.
  */
@@ -41,12 +41,32 @@ kiokuAPI bool srsGit_Repo_Close();
  */
 kiokuAPI bool srsGit_Shutdown();
 
-kiokuAPI bool srsGit_Commit(const char *path);
+/**
+ * Commit whatever has been added.
+ * @param[in] message The commit message
+ * @return Whether it was successful.
+ */
+kiokuAPI bool srsGit_Commit(const char *message);
 
-kiokuAPI bool srsGit_Add(const char *message);
+/**
+ * Add a path to be committed.
+ * @param[in] path The path(s) to be added. This can be a regex search for files to add. They must be relative to the root of the repository.
+ * @return Whether it was successful.
+ */
+kiokuAPI bool srsGit_Add(const char *path);
 
+/**
+ * Whether a path represents a valid git repository.
+ * @param[in] path The path to the repository.
+ * @return Whether it is a repository
+ */
 kiokuAPI bool srsGit_IsRepo(const char *path);
 
+/**
+ * Set the new current repository to work with.
+ * @param[in] path The path to the repository.
+ * @return Whether it was successful.
+ */
 kiokuAPI bool srsGit_Repo_Open(const char *path);
 
 #endif /* _KIOKU_SIMPLEGIT_H */
